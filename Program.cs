d6f36c5c769ca1f0f5324dce7da46ded1df3e706
Alex Shaburov <frosty174@yandex.ru>
2023-06-16 23:39:16 +0500

    added task 69

--- Program.cs
+++ Program.cs
@@ -958,3 +958,34 @@
 
 //===============================================
 
+// Задача 69: Напишите программу, которая на 
+// вход принимает два числа A и B, и возводит число
+//  А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// int Multiplication(int number1, int number2)
+// {
+//     if (number2 == 0)
+//     {
+//         return 1;
+//     }
+//     if (number2 == 1)
+//     {
+//         return number1;
+//     }
+//     // 2*(2*(2))=8
+
+//     return (number1 * Multiplication(number1, number2 - 1));
+// }
+
+// Console.WriteLine(Multiplication(num, num1));
+
+//===============================================
+
