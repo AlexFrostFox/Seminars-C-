03d6ef4887ad7c34bd8bf0f36fb822a76614b32f
Alex Shaburov <frosty174@yandex.ru>
2023-06-16 23:14:33 +0500

    added task 65

--- Program.cs
+++ Program.cs
@@ -903,4 +903,35 @@
 //     return (num1 + " " + PrintNumbers(num1 + 1, num2));
 // }
 
-//==================================================================
+//================================================
+
+// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+//Решение: 
+
+// Console.WriteLine("Введите число m");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число n");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int num1, int num2)
+// {
+//     if (num1 > num2)
+//     {
+//         return "Ошибка";
+//     }
+//     if (num1 == num2)
+//     {
+//         return num1.ToString();
+//     }
+//     return (num1 + " " + PrintNumbers(num1 + 1, num2));
+// }
+
+// Console.WriteLine(PrintNumbers(num, num1));
+
+//===============================================
+
